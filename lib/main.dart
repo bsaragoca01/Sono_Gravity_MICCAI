b1b8469c59d56a3c75bf23728298fd332b736a49
Beatriz Saragoça <166731134+bsaragoca01@users.noreply.github.com>
2024-10-29 22:42:27 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,19 @@
 import 'package:app_sono_gravity/pages/home.dart';
-import 'package:app_sono_gravity/pages/choose_location.dart';
 import 'package:app_sono_gravity/pages/loading.dart';
 import 'package:app_sono_gravity/pages/predictions.dart';
 import 'package:flutter/material.dart';
-//import 'package:app_sono_gravity/pages/test.dart';
 
 void main() {
-  // Ensures that all bindings are initialized properly before running the app
-  //WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize the ONNX Runtime environment
-  //OrtEnv.instance.init();  // If init() is synchronous, no await needed
-
-  // Run the Flutter app
+  //Run the Flutter app
   runApp(
     MaterialApp(
-
-      initialRoute: '/home',
+      initialRoute: '/home', 
       routes: {
-        '/': (context) => Home(), // '/' is the home page
+        '/': (context) => Home(), //'/' is the home page. The app initiates in this page.
         '/home': (context) => Home(),
-        '/loading': (context) => Loading(),
-        //'/choose_location': (context) => const ChooseLocation(),
-        '/predict': (context) => MyApp(),
+        '/loading': (context) => Loading(), //This page appears while wainting for the connection to be stablished and the predicted frames displayed.
+        '/predict': (context) => MyApp(), //The predictions will be displayed in this page.
       },
     ),
   );
 }
-
-
