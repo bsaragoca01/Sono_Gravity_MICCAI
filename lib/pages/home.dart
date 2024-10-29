c7905d11abd20146ef5f2d20a4d227856fde9aa9
Beatriz Saragoça <166731134+bsaragoca01@users.noreply.github.com>
2024-10-29 22:45:09 +0000

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( // The Home page design is defined here
       backgroundColor: Color(0xFFF2F2F2),
       body: Stack(
         children: [
@@ -22,18 +18,18 @@ class _HomeState extends State<Home> {
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.cover,
-                image: AssetImage('assets/logo.png'),
+                image: AssetImage('assets/logo.png'), //App logo
               ),
             ),
           ),
           Positioned(
-            bottom: 4.0, // Ajuste conforme necessário
+            bottom: 4.0,
             left: 0,
             right: 0,
             child: Center(
-              child: ElevatedButton(
+              child: ElevatedButton( //Button that when pressed the page is redirected to the Predictions Page.
                 onPressed: () {
-                  Navigator.pushNamed(context, '/predict');
+                  Navigator.pushNamed(context, '/predict'); 
                 },
                 style: ElevatedButton.styleFrom(
                   shape: CircleBorder(
@@ -44,12 +40,12 @@ class _HomeState extends State<Home> {
                   ),
                   elevation: 10,
                   shadowColor: Colors.black,
-                  padding: EdgeInsets.all(20), // Adjust padding to increase button size
-                  backgroundColor: Color(0xFFbacae7), // Apply #bacae7 as background color
+                  padding: EdgeInsets.all(20),
+                  backgroundColor: Color(0xFFbacae7),
                 ),
                 child: Image.asset(
-                  'assets/probe0.png', // Path to your image
-                  width: 60, // Adjust the size of the image
+                  'assets/probe0.png', //Image displayed in the button
+                  width: 60,
                   height: 60,
               ),
               ),
