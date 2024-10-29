00a531c21bd6c68d6dae12d9a693783a024d5eaa
Beatriz Saragoça <166731134+bsaragoca01@users.noreply.github.com>
2024-10-29 17:08:16 +0000

    Update dice_score.py

--- DeepLab/dice_score.py
+++ DeepLab/dice_score.py
@@ -5,36 +5,20 @@ import numpy as np
 import torch.nn.functional as F
 
 def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
-    assert input.size() == target.size(), "Input and target must have the same size."
-    #Ensure input and target are binary masks
-    input = (input > 0.5).float()
-    target = (target > 0.5).float()
-
-    if input.dim() == 4 and reduce_batch_first:
-        sum_dim = (1, 2, 3)  #Shape [batch, channel, height, width]
-    elif input.dim() == 4:
-        sum_dim = (1, 2, 3)
-    elif input.dim() == 3:
-        sum_dim = (1, 2)
-    else:
-        raise ValueError("Unexpected input dimensions.")
-    #Calculate intersection and union
+    # Average of Dice coefficient for all batches, or for a single mask
+    assert input.size() == target.size()
+    assert input.dim() == 3 or not reduce_batch_first
+    sum_dim = (-1, -2) if input.dim() == 2 or not reduce_batch_first else (-1, -2, -3)
     inter = 2 * (input * target).sum(dim=sum_dim)
     sets_sum = input.sum(dim=sum_dim) + target.sum(dim=sum_dim)
     sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
-    # Compute Dice coefficient
     dice = (inter + epsilon) / (sets_sum + epsilon)
-
-    if reduce_batch_first:
-        return dice.mean().item()
-    else:
-        return dice.mean(dim=0).tolist()
-
+    return dice.mean()
 
 def multiclass_dice_coeff(input: Tensor, target: Tensor, epsilon: float = 1e-7):
     #Compute the average Dice coefficient over all classes in a multi-class segmentation.
     #The epsilon represents a constant to prevent the division by zero
-
+    
     batch_size, num_classes, H, W = input.shape
     dice_scores = []
     #Ensure input and target tensors are in the same device
@@ -50,6 +34,10 @@ def multiclass_dice_coeff(input: Tensor, target: Tensor, epsilon: float = 1e-7):
         dice_scores.append(dice_score)
     return torch.stack(dice_scores).mean().item()
 
+def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
+    # Dice loss (objective to minimize) between 0 and 1
+    fn = multiclass_dice_coeff if multiclass else dice_coeff
+    return 1 - fn(input, target)
 
 def accuracy(output: torch.Tensor, target: torch.Tensor, reduce_batch_first: bool = False):
     with torch.no_grad():
