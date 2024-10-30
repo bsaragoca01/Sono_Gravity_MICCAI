f128720c5c678535c9a75d384d0c4753fca2919a
Beatriz Saragoça <166731134+bsaragoca01@users.noreply.github.com>
2024-10-30 00:50:38 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:app_sono_gravity/pages/home.dart';
-import 'package:app_sono_gravity/pages/loading.dart';
 import 'package:app_sono_gravity/pages/predictions.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +10,6 @@ void main() {
       routes: {
         '/': (context) => Home(), //'/' is the home page. The app initiates in this page.
         '/home': (context) => Home(),
-        '/loading': (context) => Loading(), //This page appears while wainting for the connection to be stablished and the predicted frames displayed.
         '/predict': (context) => MyApp(), //The predictions will be displayed in this page.
       },
     ),
